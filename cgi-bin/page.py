fd6a6672ac473e1c978b9730286af7e51505fa51
Chekuri Viroopaksh <69083163+KABALI-25@users.noreply.github.com>
2021-01-02 13:47:12 +0530

    Setting up dynamic HTML server

---
+++ cgi-bin/page.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import random
+import datetime
+
+print("Content-type:text/html\n")
+print("<!DOCTYPE html>")
+
+print('<link rel="stylesheet" type="text/css" href="style/style.css">')
+print('<div class="content">')
+print('<h1> These are your random numbers at this date and time'+str(datetime.datetime.now())+'</h1>')
+
+for i in range(10):
+    print('<p> Random Number'+str(i+1)+':'+str(random.randrange(0,1000))+'</p>')
+
+print('<p> <a href="/"> Home Page </a> </p>')
+print('</div>')
